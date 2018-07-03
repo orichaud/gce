aeef6714d1dacec112fb27082e5f023a97048b5d
Olivier Richaud <olivier@richaud.net>
2018-07-03 09:06:56 +0200

    Added comments to the code.

--- src/main/main.go
+++ src/main/main.go
@@ -11,20 +11,24 @@ import (
 	"time"
 )
 
+// This is the structure whcih contains the counter. it must be atomically incremented.
 type counter struct {
 	count uint64
 }
 
+// Simple atomic increment of the counter
 func (c *counter) Incr() uint64 {
 	return atomic.AddUint64(&c.count, 1)
 }
 
+// This is the structure which is marshalled as JSON
 type response struct {
 	Status   string `json:"status"`
 	Host     string `json:"host"`
 	Response string `json:"response,omitempty"`
 }
 
+// Simple function to send a JSON output of the response
 func (r *response) Send(w http.ResponseWriter) error {
 	data, err := json.Marshal(*r)
 	if err != nil {
@@ -34,6 +38,9 @@ func (r *response) Send(w http.ResponseWriter) error {
 	return nil
 }
 
+// Request handler. It incrmeents the counter and send a JSON response.
+// Only HTTP GET is answered, an error is otherwise returned to the caller.
+// the repsonse contains the incremented counter.
 func handleRequest(c *counter, w http.ResponseWriter, r *http.Request) {
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -65,6 +72,7 @@ func handleRequest(c *counter, w http.ResponseWriter, r *http.Request) {
 	log.Printf("Handling request: %s - %s ", r.Method, elapsed)
 }
 
+// Probe handler for liveness and readiness. Used only by Kubernetes.
 func handleLiveness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("X-Liveness", "alive")
 	w.Write([]byte("ok"))
